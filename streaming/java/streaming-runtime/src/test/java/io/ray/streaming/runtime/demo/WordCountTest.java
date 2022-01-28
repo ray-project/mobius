7976e4bbed0d8df6abcbc564b443342f7d8ae3b3
ashione <ashione@B-38L6MD6R-2326.local>
2022-01-29 01:23:31 +0800

    Fix mock consumed buf release early

--- streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/demo/WordCountTest.java
+++ streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/demo/WordCountTest.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Math;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
