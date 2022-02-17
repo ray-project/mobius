fec6ca227424b15b708c01fd11e62fa1783b9787
ashione <lingxuan.zlx@antgroup.com>
2022-02-17 16:15:04 +0800

    build streaming java dist jar

--- streaming/python/setup.py
+++ streaming/python/setup.py
@@ -6,21 +6,24 @@ from setuptools import setup, find_packages
 logger = logging.getLogger(__name__)
 file_path = os.getcwd()
 
-# build streaming package
-bazel_build_cmd = "bazel build streaming_pkg --jobs=4"
+# Build streaming package including java pkg by default
 cwd = f"{file_path}/.."
-process = subprocess.Popen(bazel_build_cmd, cwd=cwd, shell=True)
-process.wait()
+bazel_build_cmd = [
+    "bazel build streaming_pkg --jobs=4",
+    "bazel build //java:streaming_java_pkg --jobs=4"
+]
+for cmd in bazel_build_cmd:
+    process = subprocess.Popen(bazel_build_cmd, cwd=cwd, shell=True)
+    process.wait()
 
-# package raystreaming
+# Package raystreaming
 setup(
     name="raystreaming",
     version="1.0",
     author="Ray Streaming Team",
     description="streaming module",
     packages=find_packages(),
-    package_data={"raystreaming": ["_streaming.so"]},
-    install_requires=[
-        "msgpack>=0.6.2"
-    ])
-
+    package_data={
+        "raystreaming": ["_streaming.so", "jars/raystreaming_dist.jar"]
+    },
+    install_requires=["msgpack>=0.6.2"])
