995d002999fc159f10a1526a57a0fbcec30001ae
ashione <lingxuan.zlx@antgroup.com>
2022-02-21 18:08:17 +0800

    add mac os

--- scripts/install-bazel.sh
+++ scripts/install-bazel.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 # Current github bazel version is 5.0.0
-wget "https://github.com/bazelbuild/bazel/releases/download/5.0.0/bazel_5.0.0-linux-x86_64.deb" -O bazel_5.0.0-linux-x86_64.deb 
-dpkg -i bazel_5.0.0-linux-x86_64.deb
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+
+arg1="${1-}"
+
+achitecture="${HOSTTYPE}"
+platform="unknown"
+case "${OSTYPE}" in
+  msys)
+    echo "Platform is Windows."
+    platform="windows"
+    # No installer for Windows
+    ;;
+  darwin*)
+    echo "Platform is Mac OS X."
+    platform="darwin"
+    ;;
+  linux*)
+    echo "Platform is Linux (or WSL)."
+    platform="linux"
+    ;;
+  *)
+    echo "Unrecognized platform."
+    exit 1
+esac
+
+if [ "${platform}" = "darwin"]; then
+    wget "https://github.com/bazelbuild/bazel/releases/download/5.0.0/bazel-5.0.0-installer-darwin-x86_64.sh" -O bazel-5.0.0-installer-darwin-x86_64.sh
+    sh bazel-5.0.0-installer-darwin-x86_64.sh
+else
+    wget "https://github.com/bazelbuild/bazel/releases/download/5.0.0/bazel_5.0.0-linux-x86_64.deb" -O bazel_5.0.0-linux-x86_64.deb
+    dpkg -i bazel_5.0.0-linux-x86_64.deb
+fi
