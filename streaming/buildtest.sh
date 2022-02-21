ded6a7037ac74e7e2d08b876ed29a67ea646595e
ashione <lingxuan.zlx@antgroup.com>
2022-02-21 16:50:28 +0800

    add build test

---
+++ streaming/buildtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+function compile()                                                                                                                                                                     
+{
+    pushd $script_dir
+      bazel build //:streaming_lib
+    popd
+}
+
+function test_streaming_cpp() 
+{
+    pushd $script_dir
+      bazel test //:streaming_lib
+    popd
+}
+
+compile
+test_streaming_cpp
+
