7a6c3b65ecceb2ce318448a3a646d2ebffbc30ba
ashione <lingxuan.zlx@antgroup.com>
2022-02-21 16:29:30 +0800

    install lastest bazel version for risv64

--- scripts/install-bazel.sh
+++ scripts/install-bazel.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 # Current github bazel version is 5.0.0
+wget "https://github.com/bazelbuild/bazel/releases/download/5.0.0/bazel_5.0.0-linux-x86_64.deb" -O bazel_5.0.0-linux-x86_64.deb 
+dpkg -i bazel_5.0.0-linux-x86_64.deb
