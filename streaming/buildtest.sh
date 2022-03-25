107cd91cffa8480d40f035dde23860670add7c3e
Lingxuan Zuo <skyzlxuan@gmail.com>
2022-03-25 10:06:48 +0000

    Add buffer pool test ut in pipeline

--- streaming/buildtest.sh
+++ streaming/buildtest.sh
@@ -15,7 +15,7 @@ function test_streaming_cpp()
     #bazel test //:all --test_filter=basic
     # NOTE(lingxuan.zlx): unsupported host instruction of bazel on github workflow
     bazel test "streaming_message_ring_buffer_tests" "barrier_helper_tests" "streaming_message_serialization_tests" "streaming_mock_transfer" \
-    "streaming_util_tests" "streaming_perf_tests" "event_service_tests" "queue_protobuf_tests" "data_writer_tests"
+    "streaming_util_tests" "streaming_perf_tests" "event_service_tests" "queue_protobuf_tests" "data_writer_tests" "buffer_pool_tests"
     exit $?
 
     popd || exit
