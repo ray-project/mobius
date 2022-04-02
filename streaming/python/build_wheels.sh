de118eaea0ebfb27243ae0d3673569602b67a778
Lingxuan Zuo <skyzlxuan@gmail.com>
2022-04-02 15:44:22 +0000

    build python wheels on manylinux

--- streaming/python/build_wheels.sh
+++ streaming/python/build_wheels.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+script_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)
 
 # Assuming in docker "quay.io/pypa/manylinux2014_x86_64".
-
+pushd $script_dir
 # Build py36 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp36-cp36m/bin/python' /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
 auditwheel repair --plat manylinux2014_x86_64 dist/raystreaming-0.0.1-cp36-cp36m-linux_x86_64.whl
@@ -19,3 +20,5 @@ PYTHON_BIN_PATH='/opt/python/cp38-cp38/bin/python' /opt/python/cp38-cp38/bin/pyt
 auditwheel repair --plat manylinux2014_x86_64 dist/raystreaming-0.0.1-cp38-cp38-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp38-cp38/bin/python setup.py clean --all
+
+pushd
