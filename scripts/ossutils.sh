c5a2c0a2f89392c3ca580af2356ce7e6aad10084
Lingxuan Zuo <skyzlxuan@gmail.com>
2022-04-11 14:43:37 +0000

    redefine usr bin for ossutil64

--- scripts/ossutils.sh
+++ scripts/ossutils.sh
@@ -2,13 +2,13 @@
 current_dir=$(dirname "${BASH_SOURCE:-$0}")
 
 install() {
-oss_path=${1:-/usr/bin/path}
+oss_path=${1:-/usr/bin/}
 wget "https://ray-mobius-us.oss-us-west-1.aliyuncs.com/ci/linux/ossutil64" -O $oss_path/ossutil64
 chmod a+x $oss_path/ossutil64
 }
 
 copy() {
-ossutil64 -i $OSS_ID -e $OSS_HOST -k $OSS_KEY cp $1 oss://${OSS_BUCKET}${2} -r -f
+ossutil64 -i ${OSS_ID:-default-id} -e ${OSS_HOST:-default-host} -k ${OSS_KEY:-default-key} cp $1 oss://${OSS_BUCKET:-default-bucket}${2} -r -f
 } 
 
 publish_python () {
@@ -16,7 +16,7 @@ pushd $current_dir
 PYTHON_DIST_DIR=../../python/dist
 COMMIT_ID=`git rev-parse HEAD`
 if [ -d $PYTHON_DIST_DIR ] ; then
-	copy $PYTHON_DIST_DIR publish/python/$COMMIT_ID
+	copy $PYTHON_DIST_DIR /publish/python/$COMMIT_ID
 else
 	echo "Python dist not found"
 fi
@@ -24,9 +24,9 @@ popd
 }
 
 if [ $1 == "install" ] ; then
-	install;
+	install
 elif [ $1 == "cp" ] ; then
-	copy $2 $3;
+	copy $2 $3
 elif [ $1 == "publish_python" ] ; then
 	publish_python
 fi
