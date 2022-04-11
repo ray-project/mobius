173a64a64e2f93688b27dbd1245f1b9b78973c41
Lingxuan Zuo <skyzlxuan@gmail.com>
2022-04-11 04:19:27 +0000

    build wheels by virtual pyenv

--- streaming/python/build_wheels.sh
+++ streaming/python/build_wheels.sh
@@ -7,17 +7,24 @@ build_with_python_version() {
 pushd $script_dir
 PYTHON_VERSION=$1
 PYTHON3_HOME="/opt/python/${PYTHON_VERSION}"
-export PATH=$PYTHON3_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$PYTHON3_HOME/lib:$LD_LIBRARY_PATH
-export PYTHON3_BIN_PATH="/opt/python/${PYTHON_VERSION}/bin/python"
-echo "Build Python Version ${PYTHONe_BIN_PATH}"
-PYTHON_BIN_PATH=$PYTHON3_BIN_PATH PYTHON3_BIN_PATH=$PYTHON3_BIN_PATH /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel
+PYTHON3_BIN_PATH="/opt/python/${PYTHON_VERSION}/bin/python"
+if [ -d $PYTHON_VERSION ] ; then
+  echo "Reuse python version virtualenv"
+else 
+  $PYTHON3_BIN_PATH -m pip install virtualenv
+  $PYTHON3_BIN_PATH -m virtualenv -p $PYTHON3_BIN_PATH $script_dir/$PYTHON_VERSION
+fi
+. $script_dir/$PYTHON_VERSION/bin/activate
+
+bash $script_dir/../../scripts/install-ray.sh
+echo "Build Python Version ${PYTHON3_BIN_PATH}"
+python setup.py bdist_wheel
 auditwheel repair --plat manylinux2014_x86_64 dist/raystreaming-0.0.1-${PYTHON_VERSION}-linux_x86_64.whl
-bazel clean --expunge
+#bazel clean --expunge
 /opt/python/$PYTHON_VERSION/bin/python setup.py clean --all
 pushd
 }
 
 build_with_python_version cp38-cp38
-#build_with_python_verion cp36-cp36m
-#build_with_python_verion cp37-cp37m
+#build_with_python_version cp37-cp37m
+#build_with_python_version cp36-cp36m
