d2b1022e4c129bf08e911907faff57978d1ce15f
Lingxuan Zuo <skyzlxuan@gmail.com>
2022-04-13 16:20:43 +0000

    build wheels exit code and twine upload

--- streaming/python/build_wheels.sh
+++ streaming/python/build_wheels.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 script_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)
 
-# Assuming in docker "quay.io/pypa/manylinux2014_x86_64".
-
 build_with_python_version() {
 pushd $script_dir
 PYTHON_VERSION=$1
@@ -20,7 +18,12 @@ fi
 bash $script_dir/../../scripts/install-ray.sh
 python setup.py bdist_wheel
 ls dist/
-auditwheel repair --plat manylinux_2_24_x86_64 dist/raystreaming-0.0.1-${PYTHON_VERSION}-*.whl
+auditwheel repair --plat manylinux_2_27_x86_64 dist/raystreaming-0.0.1-${PYTHON_VERSION}-*.whl
+exit_code = $?
+if [ exit_code != 0 ]; then
+	echo "Exit code $exit_code"
+	exit $exit_code
+fi
 #bazel clean --expunge
 /opt/python/$PYTHON_VERSION/bin/python setup.py clean --all
 pushd
@@ -29,3 +32,6 @@ pushd
 build_with_python_version cp38-cp38
 build_with_python_version cp37-cp37m
 build_with_python_version cp36-cp36m
+
+# upload wheels to pypi
+#twine upload dist/*.whl --verbose
