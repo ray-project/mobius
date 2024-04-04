8fbbe77ed613b2e490f411398da84b2674793ceb
ashione <skyzlxuan@gmail.com>
2024-04-04 22:51:56 +0800

    assign aarch64 to arm64

--- scripts/install-bazel.sh
+++ scripts/install-bazel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Current github bazel version is 5.0.0
 platform="unknown"
+arm=`uname -a | grep -o -m 1 -e "arm" -e "aarch64" | head -n 1``
 
 case "${OSTYPE}" in
   msys)
@@ -22,8 +23,11 @@ case "${OSTYPE}" in
     ;;
 esac
 echo "platform is ${platform}"
-arm=`uname -m | grep arm`
 echo "current arch is $arm"
+
+if [ "${arm}" = "aarch64" ]; then
+    arm="arm64"
+fi
 
 if [ "${platform}" = "darwin" ] && [ "${arm}" != "arm64" ]; then
     wget "https://github.com/bazelbuild/bazel/releases/download/5.4.1/bazel-5.4.1-installer-darwin-x86_64.sh" -O bazel-5.4.1-installer-darwin-x86_64.sh
