89369f4318f9d3910828a250f3258e3a8286fe9c
ashione <skyzlxuan@gmail.com>
2024-04-04 22:21:36 +0800

    add arm64 bazel pkg install

--- scripts/install-bazel.sh
+++ scripts/install-bazel.sh
@@ -22,10 +22,20 @@ case "${OSTYPE}" in
     ;;
 esac
 echo "platform is ${platform}"
+arm=`uname -m | grep arm`
+echo $arm
 
-if [ "${platform}" = "darwin" ]; then
+if [ "${platform}" = "darwin" ] && [ "${arm}" != "arm64" ]; then
     wget "https://github.com/bazelbuild/bazel/releases/download/5.4.1/bazel-5.4.1-installer-darwin-x86_64.sh" -O bazel-5.4.1-installer-darwin-x86_64.sh
     sh bazel-5.4.1-installer-darwin-x86_64.sh
+elif [ "${platform}" = "darwin" ] && [ "${arm}" = "arm64" ]; then
+    wget "https://github.com/bazelbuild/bazel/releases/download/5.4.1/bazel-5.4.1-darwin-arm64" -O bazel-5.4.1-darwin-arm64
+    chmod a+x bazel-5.4.1-darwin-arm64
+    sh bazel-5.4.1-darwin-arm64
+elif [ "${platform}" = "linux" ] && [ "${arm}" = "arm64" ]; then
+    wget "https://github.com/bazelbuild/bazel/releases/download/5.4.1/bazel-5.4.1-linux-arm64" -O bazel_5.4.1-linux-arm64
+    chmod a+x bazel_5.4.1-linux-arm64
+    sh bazel_5.4.1-linux-arm64
 else
     wget "https://github.com/bazelbuild/bazel/releases/download/5.4.1/bazel_5.4.1-linux-x86_64.deb" -O bazel_5.4.1-linux-x86_64.deb
     dpkg -i bazel_5.4.1-linux-x86_64.deb
